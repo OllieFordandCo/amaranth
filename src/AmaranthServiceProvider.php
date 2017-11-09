f321d98a8d23cc7eb36786fdf1c4a80d48132a2f
Ruben <contact@rubenmadila.com>
2017-11-09 16:41:24 +0000

    right structure

--- src/AmaranthServiceProvider.php
+++ src/AmaranthServiceProvider.php
@@ -21,7 +21,7 @@ class AmaranthServiceProvider extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/config/amaranth.php', 'amaranth'
+            __DIR__ . '../config/amaranth.php', 'amaranth'
         );
 
         $this->app->singleton('Mdl\Amaranth\Contracts\Factory', function ($app) {
@@ -46,34 +46,34 @@ class AmaranthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '../routes/web.php');
 
         // Migrations
-        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '../database/migrations');
         $this->publishes([
-            __DIR__.'/database/migrations/' => database_path('migrations')
+            __DIR__ . '../database/migrations/' => database_path('migrations')
         ], 'migrations');
 
         // Translations
-        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'amaranth');
+        $this->loadTranslationsFrom(__DIR__ . '../resources/lang', 'amaranth');
         $this->publishes([
-            __DIR__.'/resources/lang' => resource_path('lang/vendor/amaranth'),
+            __DIR__ . '../resources/lang' => resource_path('lang/vendor/amaranth'),
         ]);
 
         // Views
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'amaranth');
+        $this->loadViewsFrom(__DIR__ . '../resources/views', 'amaranth');
         $this->publishes([
-            __DIR__.'/resources/views' => resource_path('views/vendor/amaranth'),
+            __DIR__ . '../resources/views' => resource_path('views/vendor/amaranth'),
         ]);
 
         // Config
         $this->publishes([
-            __DIR__.'/config/amaranth.php' => config_path('amaranth.php'),
+            __DIR__ . '../config/amaranth.php' => config_path('amaranth.php'),
         ]);
 
         // Assets
         $this->publishes([
-            __DIR__.'/public' => public_path('vendor/amaranth'),
+            __DIR__ . '../public' => public_path('vendor/amaranth'),
         ], 'public');
 
     }
