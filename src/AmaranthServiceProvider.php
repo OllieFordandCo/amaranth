4cb3c707734977dc51cfed4508a9a26996d4e19f
Ruben <contact@rubenmadila.com>
2017-11-09 16:59:23 +0000

    override routes

--- src/AmaranthServiceProvider.php
+++ src/AmaranthServiceProvider.php
@@ -46,35 +46,37 @@ class AmaranthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->package('OllieFordandCo/Amaranth', 'amaranth');
+
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
         // Migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->publishes([
             __DIR__ . '/../database/migrations/' => database_path('migrations')
-        ], 'migrations');
+        ], 'amaranth-migrations');
 
         // Translations
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'amaranth');
         $this->publishes([
             __DIR__ . '/../resources/lang' => resource_path('lang/vendor/amaranth'),
-        ]);
+        ], 'amaranth-translations');
 
         // Views
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'amaranth');
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/amaranth'),
-        ]);
+        ], 'amaranth-views');
 
         // Config
         $this->publishes([
             __DIR__ . '/../config/amaranth.php' => config_path('amaranth.php'),
-        ]);
+        ], 'amaranth-config');
 
         // Assets
         $this->publishes([
             __DIR__ . '/../public' => public_path('vendor/amaranth'),
-        ], 'public');
+        ], 'amaranth-public');
 
     }
 
