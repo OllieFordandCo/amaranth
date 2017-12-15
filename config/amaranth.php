1363846e8eb79bae6e5e6f5d736fef3717e4d770
Ruben <contact@rubenmadila.com>
2017-12-15 12:35:15 +0000

    settings

--- config/amaranth.php
+++ config/amaranth.php
@@ -12,6 +12,20 @@ return [
     |
     */
 
-    'analytics' => env('ENABLE_ANALYTICS', true)
+    'analytics' => env('AMARANTH_ENABLE_ANALYTICS', true),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Settings
+    |--------------------------------------------------------------------------
+    |
+    | If you want this application to capture data usage and provide a higher
+    | level of analytical view.
+    |
+    */
+
+    'allow_registrations' => env('AMARANTH_REGISTRATION', false),
+    'allow_invitations' => env('AMARANTH_BETA', true)
 
 ];
